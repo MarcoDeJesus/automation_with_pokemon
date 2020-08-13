0dcd8cfd18d70b430f1bebb59be740b9a2b7e739
CarlosRG <cramirez.griego@gmail.com>
2020-08-12 23:27:35 -0700

    Updated PokemonTemplate Class

---
+++ AutomationProject/Layer1/BaseClasses/PokemonTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonTypesNamespace
+{
+    public class PokemonTypes
+    {
+        public string TypeName;
+        public List<PokemonTypes> WeakToAttacks = new List<PokemonTypes>();
+        public List<PokemonTypes> ImmuneToAttacks = new List<PokemonTypes>();
+        public List<PokemonTypes> NeutralToAttacks = new List<PokemonTypes>();
+        public List<PokemonTypes> ResistantToAttacks = new List<PokemonTypes>();
+        public List<PokemonTypes> AttacksWeakOn = new List<PokemonTypes>();
+        public List<PokemonTypes> AttacksNeutralOn = new List<PokemonTypes>();
+        public List<PokemonTypes> AttacksStrongOn = new List<PokemonTypes>();
+        public List<PokemonTypes> AttacksWontWork = new List<PokemonTypes>();
+
+        public PokemonTypes(string type)
+        {
+            TypeName = type;
+        }
+    }
+}
