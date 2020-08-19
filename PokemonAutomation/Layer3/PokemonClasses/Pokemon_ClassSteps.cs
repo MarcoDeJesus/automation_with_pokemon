c11a7397ae98ac396abb8c19c7f4eb6f172dae26
CarlosRG <cramirez.griego@gmail.com>
2020-08-19 11:42:01 -0700

    Basic Shiny Flag test

---
+++ PokemonAutomation/Layer3/PokemonClasses/Pokemon_ClassSteps.cs
@@ -0,0 +1,63 @@
+﻿using APIModules;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace PokemonAutomation.Layer3.PokemonClasses
+{
+    [Binding]
+    public class Pokemon_ClassSteps
+    {
+        public static Dictionary<string, dynamic> TestContextData = new Dictionary<string, dynamic>();
+
+        [Given(@"the test user has selects '(.*)' as the test Pokemon")]
+        public static void GivenTheTestUserHasSelectsAsTheTestPokemon(string p0)
+        {
+            if (TestContextData.ContainsKey("TestPokemon"))
+            {
+                TestContextData.Remove("TestPokemon");
+            }
+            PokemonFactory TestPokemon = new PokemonFactory(p0.ToLower());
+            TestContextData.Add("TestPokemon", TestPokemon);
+        }
+        
+
+        [When(@"the test user generates an instance using only the IsShiny flag as '(.*)'")]
+        public static void WhenTheTestUserGeneratesAnInstanceUsingOnlyTheIsShinyFlagAs(string p0)
+        {
+            if (TestContextData.ContainsKey("TestInstance"))
+            {
+                TestContextData.Remove("TestInstance");
+            }
+            PokemonFactory TestPokemon = TestContextData["TestPokemon"];
+            switch (p0)
+            {
+                case "enabled":
+                    Pokemon Shiny = new Pokemon(TestPokemon, true);
+                    TestContextData.Add("TestInstance", Shiny);
+                    break;
+                case "disabled":
+                    Pokemon NotShiny = new Pokemon(TestPokemon, false);
+                    TestContextData.Add("TestInstance", NotShiny);
+                    break;
+            }
+        }
+        
+        [Then(@"the result Pokemon should '(.*)' be shiny")]
+        public static void ThenTheResultPokemonShouldBeShiny(string shiny)
+        {
+            Pokemon TestInstance = TestContextData["TestInstance"];
+            bool IsShiny = TestInstance.IsShiny;
+            switch (shiny)
+            {
+                case "yes":
+                    Assert.True(true.Equals(IsShiny));
+                    break;
+                case "no":
+                    Assert.True(false.Equals(IsShiny));
+                    break;
+            }
+        }
+    }
+}
---
+++ PokemonAutomation/Layer4/API/Pokemon_Class.feature
@@ -0,0 +1,15 @@
+﻿Feature: Pokemon_Class
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given the test user has selects '<name>' as the test Pokemon
+	When the test user generates an instance using only the IsShiny flag as '<state>'
+	Then the result Pokemon should '<isShiny>' be shiny
+
+	Examples: 
+	| name   | state    | isShiny |
+	| Mewtwo | enabled  | yes     |
+	| Mewtwo | disabled | no      |
