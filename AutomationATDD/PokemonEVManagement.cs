3c1ea94fb668cc39503fc26014803d978efe96ea
CarlosRG <cramirez.griego@gmail.com>
2021-01-27 20:09:07 -0700

    Added more test coverage to EV Management Class

--- AutomationATDD/PokemonEVManagement.cs
+++ AutomationATDD/PokemonEVManagement.cs
@@ -10,14 +10,15 @@ namespace AutomationATDD
         [TestCase(200, 253, 200)]
         [TestCase(200, 52, 252)]
         [TestCase(100, 52, 152)]
+        [TestCase(254, 0, 0)]
         [TestCase(-1, -1, 0)]
         public void AddingEVsIntoPokemonHPStat(int initialEV, int addedEV, int expectedEV)
         {
             EVManagement evObject = new EVManagement();
             evObject.AddEVPointsToHP(initialEV);
             evObject.AddEVPointsToHP(addedEV);
-            int points = evObject.hp;
-            Assert.True(expectedEV.Equals(points));
+            int actualEV = evObject.hp;
+            Assert.AreEqual(expectedEV, actualEV);
         }
     }
 }
