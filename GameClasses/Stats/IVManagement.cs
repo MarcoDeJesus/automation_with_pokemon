ff59ec54027b9ef50e7dae6fc9ed8b53baf6959c
CarlosRG <cramirez.griego@gmail.com>
2021-02-25 13:52:02 -0700

    Enhanced validation in IV Allocation class, defaulting to All Random Values. Created function to validate the input values. No more than 6 values allowed and they have to be between 0 and 31.

--- GameClasses/Stats/IVManagement.cs
+++ GameClasses/Stats/IVManagement.cs
@@ -29,75 +29,119 @@ namespace StatsManagement
 
         public IVManagement(bool _hp, bool _attack, bool _defense, bool _specialA, bool _specialDef, bool _speed, List<int> values)
         {
-            if (_hp)
+            if (ValidateInputArray(values))
             {
-                hp = values[0];
+                if (_hp)
+                {
+                    hp = values[0];
+                }
+                else
+                {
+                    hp = GenerateRandomIVValue();
+                }
+                if (_attack)
+                {
+                    attack = values[1];
+                }
+                else
+                {
+                    attack = GenerateRandomIVValue();
+                }
+                if (_defense)
+                {
+                    defense = values[2];
+                }
+                else
+                {
+                    defense = GenerateRandomIVValue();
+                }
+                if (_specialA)
+                {
+                    specialAttack = values[3];
+                }
+                else
+                {
+                    specialAttack = GenerateRandomIVValue();
+                }
+                if (_specialDef)
+                {
+                    specialDefense = values[4];
+                }
+                else
+                {
+                    specialDefense = GenerateRandomIVValue();
+                }
+                if (_speed)
+                {
+                    speed = values[5];
+                }
+                else
+                {
+                    speed = GenerateRandomIVValue();
+                }
             }
             else
             {
                 hp = GenerateRandomIVValue();
-            }
-            if (_attack)
-            {
-                attack = values[1];
-            }
-            else
-            {
                 attack = GenerateRandomIVValue();
-            }
-            if (_defense)
-            {
-                defense = values[2];
-            }
-            else
-            {
                 defense = GenerateRandomIVValue();
-            }
-            if (_specialA)
-            {
-                specialAttack = values[3];
-            }
-            else
-            {
                 specialAttack = GenerateRandomIVValue();
-            }
-            if (_specialDef)
-            {
-                specialDefense = values[4];
-            }
-            else
-            {
                 specialDefense = GenerateRandomIVValue();
-            }
-            if (_speed)
-            {
-                speed = values[5];
-            }
-            else
-            {
                 speed = GenerateRandomIVValue();
             }
+
         }
 
 
         public IVManagement(List<int> values)
         {
-            hp = values[0];
-            attack = values[1];
-            defense = values[2];
-            specialAttack = values[3];
-            specialDefense = values[4];
-            speed = values[5];
+            if (ValidateInputArray(values))
+            {
+                hp = values[0];
+                attack = values[1];
+                defense = values[2];
+                specialAttack = values[3];
+                specialDefense = values[4];
+                speed = values[5];
+            }
+            else {
+                hp = GenerateRandomIVValue();
+                attack = GenerateRandomIVValue();
+                defense = GenerateRandomIVValue();
+                specialAttack = GenerateRandomIVValue();
+                specialDefense = GenerateRandomIVValue();
+                speed = GenerateRandomIVValue();
+            }
         }
 
 
         private int GenerateRandomIVValue()
         {
-            Thread.Sleep(15);
+            Thread.Sleep(2);
             Random r = new Random(DateTime.Now.Millisecond);
             int rInt = r.Next(31);
             return rInt;
         }
 
+        private bool ValidateInputArray(List<int> values)
+        {
+            bool results = false;
+            if (values.Count == 6)
+            {
+                foreach (int i in values)
+                {
+                    if (i >= 0 || i <= 31)
+                    {
+                        results = true;
+                    }
+                    else 
+                    {
+                        results = false;
+                        break;
+                    }
+                }
+            }
+            return results;
+        }
     }
 }
